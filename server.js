5da131056af57c7e80269db640ddf3a5c1a2e74e
unglinh279 <unglinh2004@gmail.com>
2022-05-01 15:54:10 +0700

    nvm

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-let initial_path = path.join(__dirname, "public");
+let initial_path = path.join(__dirname, "docs");
 
 const app = express();
 app.use(express.static(initial_path));
