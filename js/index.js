df0c22283be311e9a5deb3bb35cc0d83590b83da
unglinh279 <unglinh2004@gmail.com>
2022-05-01 20:57:23 +0700

    add tag to cards

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let tag = urlParams.get('tag');
 
 const blogSection = document.querySelector('.blogs-section');
 
-if(tag != ''){
+if(tag != null){
     db.collection("blogs").where("tag", "==", tag).get().then((blogs) => {
         blogs.forEach(blog => {
             const urlParams = new URLSearchParams(window.location.search);
@@ -28,11 +28,23 @@ else{
 
 const createBlog = (blog) => {
     let data = blog.data();
+
+    let title = data.title;
+    if(title.length > 100){
+        title = title.substring(0, 100) + ' ...';
+    }
+    
+    let article = data.article;
+    if(article.length > 200){
+        article = article.substring(0, 200) + ' ...';
+    }
+
     blogSection.innerHTML += `
     <div class="blog-card">
         <img src="${data.bannerImage}" class="blog-image" alt="banner image">
-        <h1 class="blog-title">${data.title.substring(0, 100) + '...'}</h1>
-        <p class="blog-overview">${data.article.substring(0, 200) + '...'}</p>
+        <h1 class="blog-title">${title}</h1>
+        <p class="blog-overview">${article}</p>
+        <p class="blog-overview">${data.tag}</p>
         <a href="blog.html" onClick="location.href=this.href+'?id=${blog.id}';return false;" class="btn dark">read</a>
     </div>
     `;
